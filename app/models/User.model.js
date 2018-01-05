96a10059059254f9b88a780620e87956e72efd4a
manojaccion <manoj.chaurasiya@accionlabs.com>
2018-01-05 19:45:21 +0530

    user update

--- app/models/User.model.js
+++ app/models/User.model.js
@@ -5,6 +5,18 @@ const schema = mongoose.Schema;
 var UserSchema = new schema({
     first_name:String,
     last_name:String,
+
+    name:String,
+    mobile:String,
+    street:String,
+    city:String,
+    state_address:String,
+    country:String,
+    zip:String,
+    pic_url:String,
+
+
+
     email:String,
     awana_role: Array,
     church_role:String,
@@ -12,29 +24,16 @@ var UserSchema = new schema({
     access_level:String,
     status:String,
     has_personal_acct:String,
-    
-    
     last_login:String,
     gender:String,
     birthdate:String,
     home_phone:String,
-    street:String,
-    city:String,
-    state:String,
-    country:String,
-    zip:String,
-    mobile:String,
     
-    pic_url:String,
     username:String,
     password:String,
     access_token:String,
     signature:String
-    
-    // "update_url":"/userManagement/details?id=a1Y50000009TXHiEAO",
-    // "reject_url":"/userManagement/reject_affiliation?id=a1Y50000009TXHiEAO",
-    // "remove_url":"/userManagement/remove_affiliation?id=a1Y50000009TXHiEAO",
-    // "approve_url":"/userManagement/details?msg=approve&id=a1Y50000009TXHiEAO",
+
 });
 
 module.exports = mongoose.model('User',UserSchema);
--- app/routes/apirouter.js
+++ app/routes/apirouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+var promise = mongoose.connect('mongodb://localhost/awana', {useMongoClient: true});
+const bluebird = require('bluebird');
+mongoose.Promise = bluebird;
 
 const userController = require('../controllers/User.controller');
 const profileController = require('../controllers/Profile.controller');
-
-
-
 
 module.exports = router;
